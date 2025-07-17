62a8422b3a1c85e4ac1899bb8ee85a6fe37ad2ae
Javon <javonisrare123@gmail.com>
2025-07-17 13:39:42 -0500

    Added comments explaining how each version works

--- Day 5/main.py
+++ Day 5/main.py
@@ -9,6 +9,7 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 # Easy Version
+# randomly choose letters, symbols, and numbers from the lists. Then combine the entire list into a single string. Use String concatenation to create the generated password.
 # random_letters = "".join(random.sample(letters, nr_letters))
 # random_symbols = "".join(random.sample(symbols, nr_symbols))
 # random_numbers = "".join(random.sample(numbers, nr_numbers))
@@ -17,6 +18,14 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 # print(f"Your generated password is: {generated_password}")
 
 # Hard Version
+# In the hard version, we will create 3 empty strings for letters, symbols, and numbers. 
+# Then we will use for loops to iterate through the number of letters, symbols, and numbers chosen by the user by using the range function.
+# and each time we will add a random character to each string by picking a random character 
+# from each list by using the random.choice() function and using the addition assignment operator to add the character to the string.
+# Finally, we will use string concatenation to combine the letters, symbols, and numbers to create the generated password.
+# To make the password more secure, we will shuffle them by creating an empty string and using the .join() method to convert the 
+# list of characters made by the random.sample() function (which took the string and placed each character into a list at a random index because
+# we used the len function to get the length of the generated password so that it shuffled all characters) into a string.
 random_letters = ""
 random_symbols = ""
 random_numbers = ""
