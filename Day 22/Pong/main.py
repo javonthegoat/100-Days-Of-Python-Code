99ee0d925b29ce2d5a2199fa81a8a94e0f42707a
Javon <javonisrare123@gmail.com>
2025-08-03 10:09:33 -0500

    created right paddle can be moved

--- Day 22/Pong/main.py
+++ Day 22/Pong/main.py
@@ -1,8 +1,15 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong")
+screen.listen()
+
+paddle = Paddle()
+
+screen.onkey(paddle.up, "Up")
+screen.onkey(paddle.down, "Down")
 
 screen.exitonclick()
---
+++ Day 22/Pong/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+HEIGHT = 5 # 20 * HEIGHT
+MOVE = 5
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.right_paddle()
+
+    def right_paddle(self):
+        self.hideturtle()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.setheading(90)
+        self.turtlesize(stretch_len=HEIGHT)
+        self.goto(350, 0)
+        self.showturtle()
+
+    def up(self):
+        self.setpos(self.xcor(), self.ycor() + MOVE)
+
+    def down(self):
+        self.setpos(self.xcor(), self.ycor() - MOVE)
