9a5f27ef7f8b8c13ca17ee360f06fded4d4fe43b
Javon <javonisrare123@gmail.com>
2025-08-03 10:23:12 -0500

    The screen is now manually updated so the paddle instantly goes to the right side of the screen

--- Day 22/Pong/main.py
+++ Day 22/Pong/main.py
@@ -6,10 +6,13 @@ screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.listen()
+screen.tracer(0)
 
 right_paddle = RightPaddle()
 
 screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
 
-screen.exitonclick()
+game_is_on = True
+while game_is_on:
+    screen.update()
