22012840c6d5f1712ab49493d34f2e0a9a7486b2
Javon <javonisrare123@gmail.com>
2025-08-03 10:28:07 -0500

    left paddle created controlled by pressing w and s keys

--- Day 22/Pong/main.py
+++ Day 22/Pong/main.py
@@ -1,5 +1,5 @@
 from turtle import Screen
-from paddle import RightPaddle
+from paddle import RightPaddle, LeftPaddle
 
 screen = Screen()
 screen.setup(width=800,height=600)
@@ -9,9 +9,12 @@ screen.listen()
 screen.tracer(0)
 
 right_paddle = RightPaddle()
+left_paddle = LeftPaddle()
 
 screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
