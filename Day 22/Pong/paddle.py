615c22f43438add0f7645c99fa96cc50bd6ffceb
Javon <javonisrare123@gmail.com>
2025-08-03 10:27:46 -0500

    new class LeftPaddle created for a left paddle

--- Day 22/Pong/paddle.py
+++ Day 22/Pong/paddle.py
@@ -23,3 +23,24 @@ class RightPaddle(Turtle):
 
     def down(self):
         self.setpos(self.xcor(), self.ycor() - MOVE)
+
+class LeftPaddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.left_paddle()
+        
+    def left_paddle(self):
+        self.hideturtle()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.setheading(90)
+        self.turtlesize(stretch_len=HEIGHT)
+        self.goto(-350, 0)
+        self.showturtle()
+
+    def up(self):
+        self.setpos(self.xcor(), self.ycor() + MOVE)
+
+    def down(self):
+        self.setpos(self.xcor(), self.ycor() - MOVE)
